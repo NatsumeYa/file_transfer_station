f2dd44cce8584eaf33a7245e5c4f47f922e31a96
NatsumeYa <101816261+NatsumeYa@users.noreply.github.com>
2024-05-20 23:52:13 -0400

    Add files via upload

---
+++ bincode.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int flag = 1;
+    if(flag){
+        printf("flag 1: hello world\n");
+    }else{
+        printf("flag 0: sos\n");
+    }
+    return 0;
+}
